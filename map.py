fd57ea3df63d66e5ded5cd8ecf6321b12903c370
Shiva Das <sgdas@ucsd.edu>
2017-09-03 14:43:15 -0700

    l

--- map.py
+++ map.py
@@ -17,12 +17,12 @@ class GithubOAuthVarsNotDefined(Exception):
 if os.getenv('GITHUB_CLIENT_ID') == None or \
         os.getenv('GITHUB_CLIENT_SECRET') == None or \
         os.getenv('APP_SECRET_KEY') == None or \
-        os.getenv('GITHUB_ORG') == None:
+       # os.getenv('GITHUB_ORG') == None:
     raise GithubOAuthVarsNotDefined('''
       Please define environment variables:
          GITHUB_CLIENT_ID
          GITHUB_CLIENT_SECRET
-         GITHUB_ORG
+      #   GITHUB_ORG
          APP_SECRET_KEY
       ''')
 
@@ -37,7 +37,7 @@ github = oauth.remote_app(
     'github',
     consumer_key=os.environ['GITHUB_CLIENT_ID'],
     consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
-    request_token_params={'scope': 'read:org'},
+#    request_token_params={'scope': 'read:org'},
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
@@ -53,9 +53,9 @@ def get_github_oauth_token():
 def inject_logged_in():
     return dict(logged_in=('github_token' in session))
 
-@app.context_processor
-def inject_github_org():
-    return dict(github_org=os.getenv('GITHUB_ORG'))
+#@app.context_processor
+#def inject_github_org():
+ #   return dict(github_org=os.getenv('GITHUB_ORG')
 
 app.secret_key='w98fw9ef8hwe98fhwef'
 #session.clear()
@@ -92,7 +92,7 @@ def authorized():
         session['github_token'] = (resp['access_token'], '')
         session['user_data']=github.get('user').data
         github_userid = session['user_data']['login']
-        org_name = os.getenv('GITHUB_ORG')
+       # org_name = os.getenv('GITHUB_ORG')
     except Exception as e:
         session.clear()
         message = 'Unable to login: ' + str(type(e)) + str(e)
@@ -101,9 +101,9 @@ def authorized():
     
     try:
         g = Github(resp['access_token'])
-        org = g.get_organization(org_name)
+      #  org = g.get_organization(org_name)
         named_user = g.get_user(github_userid)
-        isMember = org.has_in_members(named_user)
+    #    isMember = org.has_in_members(named_user)
     except Exception as e:
         message = 'Unable to connect to Github with accessToken: ' + resp['access_token'] + " exception info: " + str(type(e)) + str(e)
         session.clear()
@@ -112,7 +112,7 @@ def authorized():
     
     if not isMember:
         session.clear() # Must clear session before adding flash message
-        message = 'Unable to login: ' + github_userid + ' is not a member of ' + org_name + \
+     #   message = 'Unable to login: ' + github_userid + ' is not a member of ' + org_name + \
           '</p><p><a href="https://github.com/logout" target="_blank">Logout of github as user:  ' + github_userid + \
           '</a></p>' 
         flash(Markup(message),'error')
