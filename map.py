f36be964bb0e12a9304f5216432507c3432e3324
Shiva Das <sgdas@ucsd.edu>
2017-09-03 21:15:51 -0700

    l

--- map.py
+++ map.py
@@ -169,7 +169,7 @@ def render_pin_result():
 		session['location1']=location1_result
 		session['location2']=location2_result
 		date_result = time.asctime( time.localtime(time.time()) )
-	mongo.db.NAME_OF_YOUR_COLLECTION.insert_one( {"Name": title_result, "Type": etype_result, "Date": month_result + " " + day_result + "'" + year_result, "Time": time_result + " " + ampm_result, "Location": location_result})
+	mongo.db.NAME_OF_YOUR_COLLECTION.insert_one( {"Name": title_result, "Type": etype_result}) #"Date": month_result" " day_result"'" + year_result, "Time": time_result" "ampm_result, "Location": location_result})
 		return render_template('pin_result.html',  title=title_result, month=month_result, day=day_result, year=year_result, time=time_result, ampm=ampm_result, etype=etype_result, location=location_result, location1=location1_result, location2=location2_result, date=date_result)
 	except ValueError:
 		return "Sorry: something went wrong."
